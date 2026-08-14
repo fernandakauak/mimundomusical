d2026d2446ee95e80e479c11362ae504b74e93c1
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-08-13 20:47:16 -0400

    Carga de imágenes en ghpages - Prueba

--- src/discos.js
+++ src/discos.js
@@ -1,13 +1,12 @@
 export const discos = [
-    { id: 1, nombre: 'Tocsin', artista: 'Xmal Deutschland', genero: 'Darkwave', anio: '1984', destacada: 'Tag für Tag', img: '/src/assets/tocsin.png' },
-    { id: 2, nombre: 'Origin of Symmetry', artista: 'Muse', genero: 'Rock Progresivo', anio: '2001', destacada: 'Micro-Cuts', img: '/src/assets/symmetry.png' },
-    { id: 3, nombre: 'That Total Age', artista: 'Nitzer Ebb', genero: 'Industrial', anio: '1987', destacada: 'Violent Playground', img: '/src/assets/total.png' },
+    { id: 1, nombre: 'Tocsin', artista: 'Xmal Deutschland', genero: 'Darkwave', anio: '1984', destacada: 'Tag für Tag', img: require('/src/assets/tocsin.png') },
+    { id: 2, nombre: 'Origin of Symmetry', artista: 'Muse', genero: 'Rock Progresivo', anio: '2001', destacada: 'Micro-Cuts', img: require('/src/assets/symmetry.png') },
+    { id: 3, nombre: 'That Total Age', artista: 'Nitzer Ebb', genero: 'Industrial', anio: '1987', destacada: 'Violent Playground', img: require('/src/assets/total.png') },
     { id: 4, nombre: 'Koloss', artista: 'Meshuggah', genero: 'Metal Progresivo', anio: '2012', destacada: 'I Am Colossus', img: '/src/assets/koloss.png' },
     { id: 5, nombre: 'This is Hardcore', artista: 'Pulp', genero: 'Alternativo', anio: '1998', destacada: 'This is Hardcore', img: '/src/assets/hardcore.png' },
     { id: 6, nombre: 'Blue Blood', artista: 'X Japan', genero: 'Metal Progresivo', anio: '1989', destacada: 'X', img: '/src/assets/blueblood.png' },
     { id: 7, nombre: 'Front by Front', artista: 'Front 242', genero: 'Industrial', anio: '1988', destacada: 'Welcome to Paradise', img: '/src/assets/frontbyfront.png' },
     { id: 8, nombre: 'Freak Perfume', artista: 'Diary of Dreams', genero: 'Darkwave', anio: '2002', destacada: 'Traumtänzer', img: '/src/assets/freakperfume.png' },
     { id: 9, nombre: 'Orchid', artista: 'Opeth', genero: 'Metal Progresivo', anio: '1995', destacada: 'The Twilight Is My Robe', img: '/src/assets/orchid.png' },
-
     { id: 10, nombre: 'You Could Have it So Much Better', artista: 'Franz Ferdinand', genero: 'Alternativo', anio: '20055', destacada: 'You Could Have it So Much Better', img: '/src/assets/youcouldhaveit.png' },
 ]
